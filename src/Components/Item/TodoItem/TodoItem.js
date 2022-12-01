0793d3302056578e150333f8631f979fe49e8042
Alina Voronaya <Alinavoronaya@mail.ru>
2022-12-01 14:16:07 +0500

    refactoring

--- src/Components/Item/TodoItem/TodoItem.js
+++ src/Components/Item/TodoItem/TodoItem.js
@@ -8,7 +8,7 @@ export const TodoItem = ({todo, changeTodo, removeTodo}) => {
             <div className="todo-item_main" onClick={() => changeTodo(todo.id)}>
                 <Check isCompleted={todo.isCompleted}/>
                 <span
-                    className={` todo-item_text ${todo.isCompleted ? "todo_item--line-through" : ""}`}>{todo.text}</span>
+                    className={` todo-item_text ${todo.isCompleted ? "todo-item--line-through" : ""}`}>{todo.text}</span>
             </div>
             <div className="trash" onClick={() => removeTodo(todo.id)}></div>
         </div>
